af55d0e70079f290607e600b516ab3098b199a29
Leonardo Villegas <leon1010@hotmail.com>
2024-11-29 13:57:50 -0500

    feat(database): add mongodb configuration and change routes functionality

--- index.js
+++ index.js
@@ -1,88 +1,71 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const Person = require("./models/person");
 
 //allows to get body data from request
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 morgan.token("request-body", (request, response) => {
   return JSON.stringify(request.body);
 });
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :request-body"
+  )
+);
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
+//#region routes
 app.get("/", (request, response) => {
   response.send("<h1>Phonebook backend!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/info", (request, response) => {
-  const totalPersons = persons.length;
   const date = new Date();
 
-  response.send(`
-    <p>Phonebook has info for ${totalPersons} people</p>
-    <p>Fecha ${date}</p>
-    `);
+  Person.find({}).then((persons) => {
+    const totalPersons = persons.length;
+    response.send(`
+        <p>Phonebook has info for ${totalPersons} people</p>
+        <p>Fecha ${date}</p>
+        `);
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  return Math.floor(Math.random() * 999999);
-};
-
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -93,7 +76,7 @@ app.post("/api/persons", (request, response) => {
 
   //check if name already exists
 
-  const personByName = persons.find(
+  /* const personByName = persons.find(
     (person) =>
       person.name.trim().toLowerCase() === body.name.trim().toLowerCase()
   );
@@ -102,22 +85,60 @@ app.post("/api/persons", (request, response) => {
     return response.status(500).json({
       error: "name must be unique",
     });
-  }
+  } */
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.use(unknownEndpoint)
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
-const PORT = process.env.PORT || 3001
+//#endregion routes
+
+//#region middlewares
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+//#endregion middlewares
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
